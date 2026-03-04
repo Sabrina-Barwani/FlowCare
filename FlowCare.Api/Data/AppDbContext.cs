c04c3da7b40543426c5ed75d27cf72574f196664
Sabrina <16s18254@utas.edu.om>
2026-03-04 09:53:59 +0400

    project setup

---
+++ FlowCare.Api/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FlowCare.Api.Entities;
+namespace FlowCare.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Branch> Branches => Set<Branch>();
+    }
+}
---
+++ FlowCare.Api/Entities/Branch.cs
@@ -0,0 +1,10 @@
+﻿namespace FlowCare.Api.Entities
+{
+    public class Branch
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string City { get; set; } = default!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
